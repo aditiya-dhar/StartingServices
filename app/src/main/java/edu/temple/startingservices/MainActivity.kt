512c50353188b30530045337fb97325a22e91a4a
adi <tut25650@temple.edu>
2025-04-09 16:26:39 -0400

    added coroutine to TimerService and onClickListener to MainActivity to exchange intents with runTimer() argument

--- app/src/main/java/edu/temple/startingservices/MainActivity.kt
+++ app/src/main/java/edu/temple/startingservices/MainActivity.kt
@@ -1,6 +1,9 @@
 package edu.temple.startingservices
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,6 +13,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        
+
+        val button = findViewById<Button>(R.id.button)
+        val editText = findViewById<EditText>(R.id.editTextNumber)
+
+        button.setOnClickListener{
+            startService(Intent(this, TimerService::class.java)
+                .apply { putExtra("timer", editText.text.toString()) })
+        }
     }
 }
--- app/src/main/java/edu/temple/startingservices/TimerService.kt
+++ app/src/main/java/edu/temple/startingservices/TimerService.kt
@@ -3,10 +3,31 @@ package edu.temple.startingservices
 import android.app.Service
 import android.content.Intent
 import android.os.IBinder
+import android.util.Log
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class TimerService : Service() {
 
-    override fun onBind(intent: Intent): IBinder {
-        TODO("Return the communication channel to the service.")
+    override fun onBind(intent: Intent?): IBinder? {
+        TODO()
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        runTimer(intent?.getStringExtra("timer")!!.toInt())
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    private fun runTimer(num : Int) {
+        CoroutineScope(Job() + Dispatchers.Default).launch {
+            for(i in num downTo 0){
+                Log.d("countdown", (i).toString())
+                delay(1000)
+            }
+            stopSelf()
+        }.start()
     }
 }
